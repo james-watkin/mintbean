4310c70664c9165b8cf21b3da822af2d6b29a128
james-watkin <jameswatkins1260@gmail.com>
2021-07-28 15:24:47 -0700

    troubleshooting pt 42

--- server/app.js
+++ server/app.js
@@ -61,7 +61,7 @@ const main = async () => {
   // console.log("DIRECTORY: ", path.resolve(__dirname, ".."));
   if (process.env.NODE_ENV === "production") {
     app.use(express.static("../web/build"));
-    app.get("/", (req, res) => {
+    app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname, "..", "web", "build", "index.html"));
     });
   }
