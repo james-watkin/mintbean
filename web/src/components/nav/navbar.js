7235d222fc46739e7e722f56233b8165e664eb15
james-watkin <jameswatkins1260@gmail.com>
2021-08-01 16:05:02 -0700

    organizing of the navbar

--- web/src/components/nav/navbar.js
+++ web/src/components/nav/navbar.js
@@ -1,29 +1,59 @@
 import React from "react";
 import { connect } from "react-redux";
 import { logout } from "../../actions/session_actions";
-import { Link } from "react-router-dom";
+import { Link as ReactLink } from "react-router-dom";
+import { Flex, Button, Box, Link } from "@chakra-ui/react";
 
 const NavBar = (props) => {
   const logoutUser = () => {
     props.logout();
   };
 
+  let right = null;
+  let left = null;
+
+  if (props.loggedIn) {
+    right = (
+      <Flex>
+        <Link>
+          <ReactLink to={"/"}>Home</ReactLink>
+        </Link>
+
+        <Link onClick={() => logoutUser()} ml={4}>
+          Logout
+        </Link>
+      </Flex>
+    );
+    left = (
+      // come back and fix this.
+      <Flex>
+        <Link>Create Room</Link>
+        <Link ml={4}>Join Room</Link>
+      </Flex>
+    );
+  } else {
+    right = (
+      <Flex>
+        <Link>
+          <ReactLink to={"/"}>Home</ReactLink>
+        </Link>
+        <Box ml={4}>
+          <Link>
+            <ReactLink to={"/login"}>Login</ReactLink>
+          </Link>
+          <Link ml={2}>
+            <ReactLink to={"/register"}>Register</ReactLink>
+          </Link>
+        </Box>
+      </Flex>
+    );
+  }
+
   return (
-    <>
-      {props.loggedIn ? (
-        <div>
-          <Link to={"/"}>Home </Link>
-          <button onClick={() => logoutUser()}>LOGOUT</button>
-        </div>
-      ) : (
-        <div>
-          <button onClick={() => logoutUser()}>LOGOUT</button>
-          <Link to={"/"}>Home </Link>
-          <Link to={"/login"}>Login</Link>
-          <Link to={"/register"}>Register</Link>
-        </div>
-      )}
-    </>
+    <Flex zIndex={1} position="sticky" top={0} bg="lightblue" p={4}>
+      <Box>{left}</Box>
+      <Box ml={"auto"}>{right}</Box>
+    </Flex>
   );
 };
 
