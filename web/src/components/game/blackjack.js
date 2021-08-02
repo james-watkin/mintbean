de94ec379529ac2872b8d5ecb653a3d3fc1967bc
james-watkin <jameswatkins1260@gmail.com>
2021-08-01 20:58:01 -0700

    cards added

---
+++ web/src/components/game/blackjack.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { Box, Button, Flex } from "@chakra-ui/react";
+
+const BlackJack = ({ info }) => {
+  return (
+    <Flex alignItems="center" justifyContent="center" h="100%">
+      <Flex
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="space-between"
+        h="100%"
+      >
+        {/* Dealer Area */}
+        <Flex flexDirection="column">
+          <h1>{info.title}</h1>
+          <Box>Cards Area</Box>
+        </Flex>
+
+        {/* User area */}
+        <Flex>
+          <Box>Card Area</Box>
+          <Box>
+            <Button>Hit</Button>
+            <Button>Stay</Button>
+          </Box>
+        </Flex>
+      </Flex>
+
+      <Flex h="100%" w="100px">
+        <Box pl={40}>DeckArea33</Box>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default BlackJack;
--- web/src/components/tables/form.js
+++ web/src/components/tables/form.js
@@ -6,63 +6,74 @@ import {
   FormControl,
   FormLabel,
   Button,
-  FormErrorMessage,
   Radio,
   RadioGroup,
   Stack,
 } from "@chakra-ui/react";
 import { Formik, Form, Field } from "formik";
+import Blackjack from "../game/blackjack";
 
 const TableForm = (props) => {
   let errors = { title: "", decks: "" };
   const [value, setValue] = useState(1);
+  const [gameStart, setgameStart] = useState(false);
+  const [gameInfo, setGameInfo] = useState({});
+  let body = null;
+
+  gameStart
+    ? (body = <Blackjack info={gameInfo} />)
+    : (body = (
+        <Formik
+          initialValues={{ decks: "", title: "" }}
+          onSubmit={async (values) => {
+            values.decks = value;
+            await setGameInfo(values);
+            await setgameStart(true);
+          }}
+        >
+          <Form>
+            <Box mt={4}>
+              <Field name="title">
+                {({ field }) => (
+                  <FormControl isInvalid={errors.title}>
+                    <FormLabel htmlFor="title">Table Title</FormLabel>
+                    <Input
+                      {...field}
+                      name="title"
+                      placeholder="title"
+                      label="title"
+                      type="title"
+                      id="title"
+                      color="white"
+                      bg="black"
+                    />
+                  </FormControl>
+                )}
+              </Field>
+            </Box>
+
+            <Box mt={8}>
+              <FormLabel htmlFor="decks">Table decks</FormLabel>
+              <RadioGroup onChange={setValue} value={value} color="white">
+                <Stack direction="row">
+                  <Radio value="1">One Deck</Radio>
+                  <Radio value="3">Three Decks</Radio>
+                  <Radio value="5">Five Decks</Radio>
+                </Stack>
+              </RadioGroup>
+            </Box>
+
+            <Button mt={4} type="submit" colorScheme="teal">
+              Create Table
+            </Button>
+          </Form>
+        </Formik>
+      ));
 
   return (
     <Box w="1200px" h="600px" mt={20} bg="slategray" p={10}>
-      <Formik
-        initialValues={{ decks: "", title: "" }}
-        onSubmit={async (values) => {
-          values.decks = value;
-          console.log(values);
-        }}
-      >
-        <Form>
-          <Box mt={4}>
-            <Field name="title">
-              {({ field }) => (
-                <FormControl isInvalid={errors.title}>
-                  <FormLabel htmlFor="title">Room Title</FormLabel>
-                  <Input
-                    {...field}
-                    name="title"
-                    placeholder="title"
-                    label="title"
-                    type="title"
-                    id="title"
-                    color="white"
-                    bg="black"
-                  />
-                </FormControl>
-              )}
-            </Field>
-          </Box>
-
-          <Box mt={8}>
-            <FormLabel htmlFor="decks">Room decks</FormLabel>
-            <RadioGroup onChange={setValue} value={value} color="white">
-              <Stack direction="row">
-                <Radio value="1">One Deck</Radio>
-                <Radio value="3">Three Decks</Radio>
-                <Radio value="5">Five Decks</Radio>
-              </Stack>
-            </RadioGroup>
-          </Box>
-
-          <Button mt={4} type="submit" colorScheme="teal">
-            Create Table
-          </Button>
-        </Form>
-      </Formik>
+      {" "}
+      {body}{" "}
     </Box>
   );
 };
